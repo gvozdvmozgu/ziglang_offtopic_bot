9531583d25fa19fc277ce7b32c171434a9791305
gvozdvmozgu <gvozdvmozgu@gmail.com>
2023-09-20 12:59:25 -0700

    improved code style

--- src/main.zig
+++ src/main.zig
@@ -4,49 +4,49 @@ const json = std.json;
 
 const TELEGRAM_TOKEN = "";
 const TELEGRAM_CHAT_ID = "";
-const TELEGRAM_API_URL = "https://api.telegram.org/bot" ++ TELEGRAM_TOKEN ++ "/sendMessage";
-const COINGECKO_API_ENDPOINT = "https://api.coingecko.com/api/v3/simple/price?ids=the-open-network&vs_currencies=usd";
+const TELEGRAM_API_URL = std.Uri.parse("https://api.telegram.org/bot" ++ TELEGRAM_TOKEN ++ "/sendMessage") catch unreachable;
+const COINGECKO_API_ENDPOINT = std.Uri.parse("https://api.coingecko.com/api/v3/simple/price?ids=the-open-network&vs_currencies=usd") catch unreachable;
 
-fn fetch_toncoin_price_from_coingecko(allocator: std.mem.Allocator) !f64 {
+fn fetchToncoinPriceFromCoingecko(allocator: std.mem.Allocator) !f64 {
     var client = http.Client{ .allocator = allocator };
     defer client.deinit();
 
-    const uri = std.Uri.parse(COINGECKO_API_ENDPOINT) catch unreachable;
-
-    var request = try client.request(.GET, uri, .{ .allocator = allocator }, .{});
+    var request = try client.request(.GET, COINGECKO_API_ENDPOINT, .{ .allocator = allocator }, .{});
     defer request.deinit();
 
     try request.start();
     try request.wait();
 
-    const responseBody = request.reader().readAllAlloc(allocator, 8192) catch unreachable;
-    defer allocator.free(responseBody);
+    const body = request.reader().readAllAlloc(allocator, 8192) catch unreachable;
+    defer allocator.free(body);
 
-    const json_response = try json.parseFromSlice(json.Value, allocator, responseBody, .{});
-    defer json_response.deinit();
+    const value = try json.parseFromSlice(json.Value, allocator, body, .{});
+    defer value.deinit();
 
-    return json_response.value.object.get("the-open-network").?.object.get("usd").?.float;
+    return value.value.object.get("the-open-network").?.object.get("usd").?.float;
 }
 
-fn send_telegram_message(allocator: std.mem.Allocator, message_text: []const u8) !void {
+fn sendTelegramMessage(allocator: std.mem.Allocator, text: []const u8) !void {
     var client = http.Client{ .allocator = allocator };
-    defer client.deinit();
 
-    var uri = std.Uri.parse(TELEGRAM_API_URL) catch unreachable;
-    uri.query = std.fmt.allocPrint(allocator, "chat_id={s}&text={s}", .{ TELEGRAM_CHAT_ID, message_text }) catch unreachable;
+    var uri = TELEGRAM_API_URL;
+    uri.query = std.fmt.allocPrint(allocator, "chat_id={s}&text={s}", .{ TELEGRAM_CHAT_ID, text }) catch unreachable;
 
     var request = try client.request(.POST, uri, .{ .allocator = allocator }, .{});
-    defer request.deinit();
 
     try request.start();
     try request.wait();
+
+    defer client.deinit();
+    defer request.deinit();
 }
 
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
-    const toncoin_price = try fetch_toncoin_price_from_coingecko(allocator);
-    const formatted_message = try std.fmt.allocPrint(allocator, "The current price of Toncoin (The Open Network) is: ${d}", .{toncoin_price});
-    defer allocator.free(formatted_message);
 
-    try send_telegram_message(allocator, formatted_message);
+    const toncoin_price = try fetchToncoinPriceFromCoingecko(allocator);
+    const text = try std.fmt.allocPrint(allocator, "The current price of Toncoin (The Open Network) is: ${d}", .{toncoin_price});
+    try sendTelegramMessage(allocator, text);
+
+    defer allocator.free(text);
 }
