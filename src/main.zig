eb6fbbd7708ea5260e99a767a84f849ba6c8cbb1
gvozdvmozgu <gvozdvmozgu@gmail.com>
2024-03-17 19:58:17 +0200

    bruh

--- src/main.zig
+++ src/main.zig
@@ -6,10 +6,7 @@ pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
 
-    defer {
-        const deinit_status = gpa.deinit();
-        if (deinit_status == .leak) unreachable else {}
-    }
+    defer std.debug.assert(gpa.deinit() == .ok);
 
     const token = std.process.getEnvVarOwned(allocator, "TELEGRAM_TOKEN") catch unreachable;
     defer allocator.free(token);
